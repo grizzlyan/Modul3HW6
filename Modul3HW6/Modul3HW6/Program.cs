98a38b0fdb1abd25029fc106f2ba9db8bf44cb4c
Grizzlyan <grizzlyan140@gmail.com>
2021-08-18 08:57:57 +0300

    gitignore

---
+++ Modul3HW6/Modul3HW6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modul3HW6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
